36fc7e787f0c767f83db025d2b320c3f75a19c4c
Liu Ya <1522962872@qq.com>
2019-09-06 17:55:24 +0800

    vue-ts

---
+++ vue-ts/src/types/todo.ts
@@ -0,0 +1,5 @@
+// 定义接口定义数据格式
+export interface ITodo {
+  text: string;
+  complete: boolean;
+}
---
+++ vue-ts/src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div v-for="(list, index) in lists" :key="index">
+      <TodoItem :item="list" :index="index"></TodoItem>
+    </div>
+    <div>{{count}}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import TodoItem from '../components/TodoItem'
+import {ITodo} from '../types/todo'
+@Component({
+  name: 'home',
+  components: {
+    TodoItem,
+  },
+})
+export default class Home extends Vue {
+  // ts中类需要有修饰符  public private protected
+  // data上数据
+  // 在这里有两种写法  1. lists:ITodo[]  2. llists:Array<ITodo>
+  public lists: Array<ITodo> = [{
+    text: '吃饭',
+    complete: false,
+  }, {
+    text: '睡觉',
+    complete: true,
+  }]
+  public mounted() {
+    console.log(this.lists);
+  }
+  // 计算属性 （computed）
+  get count() {
+    return this.lists.length;
+  }
+}
+</script>
